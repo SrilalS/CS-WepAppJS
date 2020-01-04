62dcae98fb3bb48c81921f42362b489ad502c8e5
SrilalS <Srilalsachintha@Gmail.com>
2020-01-04 21:47:32 +0530

    fix

--- EpsteinDrive.js
+++ EpsteinDrive.js
@@ -1,14 +1,65 @@
 var Payload;
-var Json;
+var Jsn;
 var IDList = [];
 var i = 0;
 
+var Pname = document.querySelector("#pname");
+var cslvl = document.querySelector("#cslvl");
+var img = document.querySelector("#img");
+console.log(img.innerHTML);
+var requestOptions = {
+    method: 'GET',
+    redirect: 'follow'
+};
+
+function fetcher() {
+    fetch("https://api.splashthat.com/events/457584143/guestlist?page=1&limit=1&status%5B%5D=checkin_yes&sort=checked_in+ASC&viewGroups%5B%5D=contactTags&viewGroups%5B%5D=contactLists&viewGroups%5B%5D=bounceInfo&viewGroups%5B%5D=groupContactAnswer&viewGroups%5B%5D=groupContactEventList&viewGroups%5B%5D=contactList&access_token=Y2I0OTJjYjdkOWVhMmJhNTUyOWI0OWU5Nzk3ZmEzZTdkZGNkNzk4NTFhZTRkYzY2ZWI2YjdlNGQxZWM1NWRlZg", requestOptions)
+        .then(response => response.text())
+        .then(result => Payload = result)
+        .then(result => Jsn = JSON.parse(Payload))
+        .catch(error => console.log('Error: ', error));
+
+}
+
+
+
+
+function DataOps() {
+    console.log(Jsn.data.guests[0].contact.id);
+    for (let index = 0; index < 5; index++) {
+        var idcheck = Jsn.data.guests[0].answers[index].question_id;
+        if (idcheck == '1039306') {
+            lvl = Jsn.data.guests[0].answers[index].answer;
+            console.log(lvl);
+            cslvl.innerHTML = 'Crowdsource Level : ' + lvl;
+        } else {
+            cslvl.innerHTML = ' ';
+        };
+    };
+    id = Jsn.data.guests[0].contact.id;
+    Fname = Jsn.data.guests[0].contact.first_name;
+    Lname = Jsn.data.guests[0].contact.last_name;
+    img.src = './assets/propics/' + id + '.jpg';
+    Pname.innerHTML = Fname + ' ' + Lname;
+    //console.log(Pname.innerHTML);
+    console.log(Fname);
+    console.log(Lname);
+    console.log(id);
+}
+
 function totalLoop() {
+
+    fetcher();
+
+    setTimeout(function() {
+        DataOps();
+    }, 5 * 1000);
+
     setTimeout(function() {
         i++;
         console.log(i);
         IDList.push(i);
-        $('#div1').show();
+        $('#div1').slideDown(0);
         $('#div1').addClass('animX');
         $('#div2').removeClass('animX');
     }, 1 * 1000);
@@ -17,7 +68,7 @@ function totalLoop() {
         i++;
         console.log(i);
         IDList.push(i);
-        $('#div1').hide();
+        $('#div1').slideUp(0);
         $('#div2').addClass('animX');
     }, 11 * 1000);
 
@@ -39,47 +90,14 @@ function totalLoop() {
     }, 25 * 1000)
 }
 
-var requestOptions = {
-    method: 'GET',
-    redirect: 'follow'
-};
 
-function fetcher() {
-    fetch("https://api.splashthat.com/events/457584143/guestlist?page=1&limit=1&status%5B%5D=checkin_yes&sort=checked_in+ASC&viewGroups%5B%5D=contactTags&viewGroups%5B%5D=contactLists&viewGroups%5B%5D=bounceInfo&viewGroups%5B%5D=groupContactAnswer&viewGroups%5B%5D=groupContactEventList&viewGroups%5B%5D=contactList&access_token=M2YxYTFkODc2MGRiNmU2ZWNhNDIyMjFkMTk5ODc5M2RjZDNmNzliYjg4ZDc2NjVkYjhlMzczN2JlNzBmZDdhYw", requestOptions)
-        .then(response => response.text())
-        .then(result => Payload = result)
-        .then(result => Json = JSON.parse(Json))
-        .catch(error => console.log('Error: ', error));
-}
-
-var name = document.querySelector("#name");
-var cslvl = document.querySelector("#cslvl");
-var img = document.querySelector("#img");
-
-
-function DataOps() {
-    for (let index = 0; index < 5; index++) {
-        var idcheck = Json.data.guests[0].answers[index].question_id;
-        if (idcheck == '1039306') {
-            lvl = Json.data.guests[0].answers[index].answer;
-            console.log(lvl);
-            cslvl.innerHTML = 'Crowdsource Level : ' + lvl;
-            break;
-        };
-    };
-    id = Json.data.guests[0].id
-    Fname = Json.data.guests[0].contact.first_name;
-    Lname = Json.data.guests[0].contact.last_name;
-    img.src = './assets/img/' + id + '.png';
-    name.innerHTML = Fname + ' ' + Lname;
-    console.log(Fname);
-    console.log(Lname);
-    console.log(id);
-}
 
 totalLoop();
 
-/* 
+
+/*
+
+
 var json;
 var JO;
 
@@ -104,24 +122,10 @@ var Fname;
 var Lname;
 var lvl = 0;
 
-use include() to see if the array has it.
 
 
 
-$('input').click(function(ev) {
-    $('div').toggleClass('animX');
-    setTimeout(function() {
 
-        setTimeout(function() {
-            $('div').toggleClass('animX');
-            $('#div1').slideToggle(0);
-            $('div').addClass('animX');
-        }, 1000);
-        setTimeout(function() {
-            $('#div2').toggleClass('animX');
-        }, 1000);
-    }, 2000);
-});
 
 function timer() {
 
